ee7f0c078be6ef27e93fec152137439c9e3ddf41
Johan Guldmyr <martbhell@protonmail.com>
2023-04-15 11:29:47 +0300

    Adjust links on every page

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,14 +33,13 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+         ('Was There an NHL Game Yesterday?', 'https://wtangy.se'),
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('martbhell', 'https://twitter.com/martbhell'),
+          )
 
 DEFAULT_PAGINATION = 10
 
