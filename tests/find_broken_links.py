e3c66e74030c47fc140e013efab6201028ae0f11
Johan Guldmyr <martbhell@protonmail.com>
2023-04-17 11:32:48 +0300

    Crawl Front page for errors

---
+++ tests/find_broken_links.py
@@ -0,0 +1,23 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://guldmyr.com"
+
+response = requests.get(url)
+if response.status_code != 200:
+    print("Error: Status code", response.status_code)
+    exit(1)
+
+soup = BeautifulSoup(response.content, "html.parser")
+links = soup.find_all("a")
+
+for link in links:
+    href = link.get("href")
+    if href and href.startswith("http") and "guldmyr.com" in href:
+        response = requests.get(href)
+        if response.status_code != 200:
+            print("Error: Broken link", href)
+            exit(1)
+        if response.status_code == 200:
+            print("OK LINK", href)
