9f0fdb52c2ba98ee425756c512bc637d4eb89f40
Johan Guldmyr <martbhell@protonmail.com>
2023-04-23 14:14:03 +0300

    Tests: More tests of broken links before merging?

--- tests/find_broken_links.py
+++ tests/find_broken_links.py
@@ -2,7 +2,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://guldmyr.com"
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--host", required=True,
+                            help="URL to query")
+ARGS = parser.parse_args()
+
+url = ARGS.host
 
 response = requests.get(url)
 if response.status_code != 200:
@@ -14,7 +21,7 @@ links = soup.find_all("a")
 
 for link in links:
     href = link.get("href")
-    if href and href.startswith("http") and "https://guldmyr.com" in href:
+    if href and href.startswith("http") and url in href:
         response = requests.get(href)
         if response.status_code != 200:
             print("Error: Broken link", href)
