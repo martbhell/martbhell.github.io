e433ecec58d3d9bb3cad55d010c89ed7de9bcbfa
Johan Guldmyr <martbhell@protonmail.com>
2023-05-03 20:55:39 +0300

    tests: Not allowing symlinks in output/ dir

--- tests/test_symlinks.py
+++ tests/test_symlinks.py
@@ -3,31 +3,68 @@
 import os
 import glob
 
-content_dir = 'content'
 
-debug = False
+def test_content_dir():
+    content_dir = "content"
 
-EXCLUDE = [ 'images', 'MAKESYMLINKS.py', 'extras' ]
+    debug = False
 
-# Get a list of all .md files in the posts directory and its subdirectories
+    EXCLUDE = ["images", "MAKESYMLINKS.py", "extras"]
 
-content_files = glob.glob(os.path.join(content_dir, f'**/*'), recursive=True)
+    # Get a list of all .md files in the posts directory and its subdirectories
 
-if len(content_files) == 0:
-    print(f"ERROR: Could not find any files where is {content_dir}")
-    os._exit(1)
+    content_files = glob.glob(os.path.join(content_dir, f"**/*"), recursive=True)
 
-# Create symlinks for each file in the content directory
-BAD = [ ]
-for c_file in content_files:
-    filename = os.path.basename(c_file)
-    if filename not in EXCLUDE:
-        if not os.path.islink(c_file):
-            BAD.append(c_file)
+    if len(content_files) == 0:
+        print(f"ERROR: Could not find any files where is {content_dir}")
+        os._exit(1)
 
-if len(BAD) > 0:
-    print(f"ERROR: Only symlinks in here yo or exclude these!")
-    print(f"{BAD}")
-    os._exit(2)
-else:
-    print(f"OK: directory {content_dir} is OK")
+    # Create symlinks for each file in the content directory
+    BAD = []
+    for c_file in content_files:
+        filename = os.path.basename(c_file)
+        if filename not in EXCLUDE:
+            if not os.path.islink(c_file):
+                BAD.append(c_file)
+
+    if len(BAD) > 0:
+        print(f"ERROR: Only symlinks in here yo or exclude these!")
+        print(f"{BAD}")
+        os._exit(2)
+    else:
+        print(f"OK: directory {content_dir} is OK")
+
+
+def test_output_dir():
+    content_dir = "output"
+
+    debug = False
+
+    EXCLUDE = []
+
+    # Get a list of all files under output
+
+    content_files = glob.glob(os.path.join(content_dir, f"**/*"), recursive=True)
+
+    if len(content_files) == 0:
+        print(f"ERROR: Could not find any files where is {content_dir}")
+        os._exit(1)
+
+    # Create symlinks for each file in the content directory
+    BAD = []
+    for c_file in content_files:
+        filename = os.path.basename(c_file)
+        if filename not in EXCLUDE:
+            if os.path.islink(c_file):
+                BAD.append(c_file)
+
+    if len(BAD) > 0:
+        print(f"ERROR: Only symlinks in here yo or exclude these!")
+        print(f"{BAD}")
+        os._exit(3)
+    else:
+        print(f"OK: directory {content_dir} is OK")
+
+
+test_content_dir()
+test_output_dir()
