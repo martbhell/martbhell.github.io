9e30ff046b520eb47e3a7b86b13474c1dd85674d
Johan Guldmyr <martbhell@protonmail.com>
2023-07-16 09:32:57 +0300

    build script
    meaning we could run some more commands before deploying
    
    like making the symlinks and stuff!
    
     - #36

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+build_content () {
+  echo "branch: $CF_PAGES_BRANCH"
+  echo "site_url: $SITE_URL"
+  pelican content
+}
+
+if [ "$CF_PAGES_BRANCH" == "production" ]; then
+  # Run the "production" script in `package.json` on the "production" branch
+  # "production" should be replaced with the name of your Production branch
+
+  build_content
+
+elif [ "$CF_PAGES_BRANCH" == "staging" ]; then
+  # Run the "staging" script in `package.json` on the "staging" branch
+  # "staging" should be replaced with the name of your specific branch
+
+  build_content
+
+else
+  # Else run the dev script
+
+  build_content
+
+fi
