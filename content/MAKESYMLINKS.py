492689546a93c2338721808e11049ce24ea12900
Johan Guldmyr <martbhell@protonmail.com>
2023-07-16 22:21:18 +0300

    Also copy images to output/images from posts/yyyy..
     - #36

--- content/MAKESYMLINKS.py
+++ content/MAKESYMLINKS.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python
+""" Making all the links and copies files """
 
 import os
+import shutil
 import glob
 
 import argparse
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--test", action="store_true", help="only test and fail if a link is missing")
+parser.add_argument(
+    "--test", action="store_true", help="only test and fail if a link is missing"
+)
 args = parser.parse_args()
 
 test_mode = args.test
 
-posts_dir = '../posts'
-content_dir = '.'
-image_dir = './images'
-extras_dir = './extras'
-extras_source_dir = '../extras'
+posts_dir = "../posts"
+content_dir = "."
+image_dir = "./images"
+output_image_dir = "../output/images"
+extras_dir = "./extras"
+extras_source_dir = "../extras"
 
 debug = False
 
 # Get a list of all .md files in the posts directory and its subdirectories
 
-image_extensions = ['gif', 'jpg', 'png', 'GIF', 'JPG', 'PNG', 'webp', 'WEBP']
-md_files = glob.glob(os.path.join(posts_dir, '**/*.md'), recursive=True)
-extras_files = glob.glob(os.path.join(extras_source_dir, '*'))
+image_extensions = ["gif", "jpg", "png", "GIF", "JPG", "PNG", "webp", "WEBP"]
+md_files = glob.glob(os.path.join(posts_dir, "**/*.md"), recursive=True)
+extras_files = glob.glob(os.path.join(extras_source_dir, "*"))
 
 image_files = []
 for extension in image_extensions:
-  image_files += glob.glob(os.path.join(posts_dir, f'**/*.{extension}'), recursive=True)
+    image_files += glob.glob(
+        os.path.join(posts_dir, f"**/*.{extension}"), recursive=True
+    )
 
 posts = []
 
@@ -53,8 +61,8 @@ for md_file in md_files:
         # Create the symlink
         os.symlink(md_file, symlink_path)
     except FileExistsError:
-        if debug: print(f"Skipping {md_file}: symlink already exists.")
-        pass
+        if debug:
+            print(f"Skipping {md_file}: symlink already exists.")
 
 # Create symlinks for each image in the content directory
 for image_file in image_files:
@@ -67,7 +75,8 @@ for image_file in image_files:
 
     # Construct the symlink path by joining the content directory and the symlink filename
     symlink_path = os.path.join(image_dir, symlink_filename)
-    image_file_path = os.path.join('..', image_file)
+    image_file_path = os.path.join("..", image_file)
+    output_image_file_path = os.path.join(output_image_dir, symlink_filename)
 
     if not os.path.islink(symlink_path) and test_mode:
         print(f"ERROR: {symlink_path} is not a link")
@@ -76,8 +85,13 @@ for image_file in image_files:
         # Create the symlink
         os.symlink(image_file_path, symlink_path)
     except FileExistsError:
-        if debug: print(f"Skipping {image_file}: symlink already exists.")
-        pass
+        if debug:
+            print(f"Skipping {image_file}: symlink already exists.")
+
+    # Copy image into output/images preserving metadata
+    if not os.path.exists(output_image_file_path):
+        shutil.copy2(image_file, output_image_file_path)
+        print(f"copied {image_file} to {output_image_file_path}")
 
 # Create symlinks for each extras
 for extra_file in extras_files:
@@ -86,7 +100,7 @@ for extra_file in extras_files:
 
     # Construct the symlink path by joining the content directory and the symlink filename
     symlink_path = os.path.join(extras_dir, symlink_filename)
-    extra_file_path = os.path.join('..', extra_file)
+    extra_file_path = os.path.join("..", extra_file)
     if not os.path.islink(symlink_path) and test_mode:
         print(f"ERROR: {symlink_path} is not a link")
         os._exit(3)
@@ -95,5 +109,5 @@ for extra_file in extras_files:
         # Create the symlink
         os.symlink(extra_file_path, symlink_path)
     except FileExistsError:
-        if debug: print(f"Skipping {extra_file}: symlink already exists.")
-        pass
+        if debug:
+            print(f"Skipping {extra_file}: symlink already exists.")
